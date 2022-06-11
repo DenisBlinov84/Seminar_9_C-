864a47cf15246b077948994ff8667a52430da926
Denis <blinovda1@mail.ru>
2022-06-11 17:09:11 +0300

    add task 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая будет принимать на вход число
+//    и возвращать сумму его цифр. Использовать рекурсию.
+
+
+int SumNum(int num)
+{
+    if (num == 0) return 0;
+    return SumNum(num / 10) + num % 10;    
+}
+
+Console.WriteLine(SumNum(453));
+Console.WriteLine(SumNum(45));
