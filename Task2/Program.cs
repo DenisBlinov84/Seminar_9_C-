30cea823bea9cd31989feb8c960e58aab57c4fc5
Denis <blinovda1@mail.ru>
2022-06-11 17:07:13 +0300

    add task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Задайте значения M и N. Напишите программу, которая 
+//    рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+
+void Spacing(int M, int N)
+{
+    if (N < M) return;
+    Spacing(M, N - 1);
+    Console.Write($"{N}, ");
+}
+
+Spacing(1, 5);
